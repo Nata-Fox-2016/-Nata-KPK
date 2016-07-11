2019f80d57796ecee220e5a8bb4b40b89c9a715a
Admin <natalya.ti@mail.ru>
2016-07-11 15:33:30 +0400

    Две тренеровочные задачи для Чертежника.

---
+++ task_dm2.py
@@ -0,0 +1,14 @@
+from drawman import *
+from time import sleep
+
+def f(x)
+    return x*x
+
+
+pen_down()
+for x,y in range(0,11):
+    to_point(x, f(x))
+to_point (A[0][0], A[0][1])
+pen_up()
+
+sleep(20)
