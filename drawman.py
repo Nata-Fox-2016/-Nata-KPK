8b05917e13a657503402c17721fa3edca5194536
Admin <natalya.ti@mail.ru>
2016-07-14 15:47:58 +0400

    Еще раз сверенные программы по вашей лекции

--- drawman.py
+++ drawman.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 default_scale = 10
-
 def init_drawman():
     global t, x_current, y_current, _drawman_scale
     t = Turtle()
@@ -9,12 +8,9 @@ def init_drawman():
     y_current=0
     t.goto(x_current, y_current)
     drawman_scale(default_scale)
-
-
 def drawman_scale(scale):
     global _drawman_scale
     _drawman_scale = scale
-
 def test_drawman():
     """
     ???????????? ?????? ??????????
@@ -26,26 +22,17 @@ def test_drawman():
         on_vector(0,-20)
     pen_up
     to_point(0,0)
-
 def pen_down():
     t.pendown()
-
-
 def pen_up():
     t.penup()
-
-
 def on_vector(dx, dy):
     to_point(x_current + dx, y_current + dy)
-
-
 def to_point(x, y):
     global x_current, y_current
     x_current = x
     y_current = y
     t.goto(_drawman_scale*x_current, _drawman_scale*y_current)
-
-
 init_drawman()
 if _name_ == '_main_':
     import time
